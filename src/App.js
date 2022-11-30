c7cab17305a98aa10707da781c4d2a6bb1c610a3
mersedee <mersede.rahbar@gmail.com>
2022-11-30 17:46:23 +0330

    add board

--- src/App.js
+++ src/App.js
@@ -1,28 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import checkWinner from './helper/checkWinner';
 import './App.css';
 
+const boardInfo = {
+  rows: 6,
+  cols: 7,
+  player1: 'red',
+  player2: 'blue',
+};
+
+const createBoard = () => new Array(boardInfo.rows).fill(null).map(
+  () => new Array(boardInfo.cols).fill(null),
+);
+
+const getColor = (value) => {
+  if (value === boardInfo.player1) return 'bg-red-300';
+  if (value === boardInfo.player2) return 'bg-blue-300';
+  return 'bg-white';
+};
+
 function App() {
+  const [board, setBoard] = useState(createBoard());
+  const [currentPlayer, setCurrentPlayer] = useState(boardInfo.player1);
+  const [winner, setWinner] = useState(null);
+
+  const play = (rowIndex, colIndex) => {
+    const current = (currentPlayer === boardInfo.player1) ? boardInfo.player2 : boardInfo.player1;
+    setCurrentPlayer(current);
+    board[rowIndex][colIndex] = currentPlayer;
+  };
+
+  const resetPlay = () => {
+    setBoard(createBoard());
+    setCurrentPlayer(boardInfo.player1);
+    setWinner(null);
+  };
+
+  useEffect(() => {
+    const win = checkWinner(board);
+    if (win) setWinner(win);
+  }, [currentPlayer]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-20">
+      <h1 className="text-4xl text-center font-medium first-letter:uppercase">
+        {currentPlayer}
+        &apos;s turn
+      </h1>
+
+      <div className="bg-gray-200 w-fit block mx-auto py-3 px-2 rounded-3xl mt-8">
+        {board.map((rows, rowIndex) => (
+          <div className="flex">
+            {rows.map((col, colIndex) => (
+              <div className="h-24 w-24 flex justify-center items-center">
+                <button
+                  type="button"
+                  className={`h-20 w-20 bg-white rounded-full m-2 flex justify-center items-center ${getColor(col)}`}
+                  onClick={() => play(rowIndex, colIndex)}
+                />
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+
+      {winner && (
+        <div className="text-center">
+          <div className="text-2xl first-letter:uppercase mt-8">
+            {winner}
+            {' '}
+            is winner!
+          </div>
+          <button
+            type="button"
+            className="text-xl rounded-lg mt-8 px-4 py-3 bg-blue-500 text-white"
+            onClick={resetPlay}
+          >
+            Play Again
+          </button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/helper/checkWinner.js
@@ -0,0 +1,86 @@
+const checkVertical = (board) => {
+  // Check only if row is 3 or greater
+  for (let r = 3; r < 6; r++) {
+    for (let c = 0; c < 7; c++) {
+      if (board[r][c]) {
+        if (board[r][c] === board[r - 1][c]
+            && board[r][c] === board[r - 2][c]
+            && board[r][c] === board[r - 3][c]) {
+          return board[r][c];
+        }
+      }
+    }
+  }
+};
+
+const checkHorizontal = (board) => {
+  // Check only if column is 3 or less
+  for (let r = 0; r < 6; r++) {
+    for (let c = 0; c < 4; c++) {
+      if (board[r][c]) {
+        if (
+          board[r][c] === board[r][c + 1]
+          && board[r][c] === board[r][c + 2]
+          && board[r][c] === board[r][c + 3]
+        ) {
+          return board[r][c];
+        }
+      }
+    }
+  }
+};
+
+const checkDiagonalRight = (board) => {
+  // Check only if row is 3 or greater AND column is 3 or less
+  for (let r = 3; r < 6; r++) {
+    for (let c = 0; c < 4; c++) {
+      if (board[r][c]) {
+        if (
+          board[r][c] === board[r - 1][c + 1]
+          && board[r][c] === board[r - 2][c + 2]
+          && board[r][c] === board[r - 3][c + 3]
+        ) {
+          return board[r][c];
+        }
+      }
+    }
+  }
+};
+
+const checkDiagonalLeft = (board) => {
+  // Check only if row is 3 or greater AND column is 3 or greater
+  for (let r = 3; r < 6; r++) {
+    for (let c = 3; c < 7; c++) {
+      if (board[r][c]) {
+        if (
+          board[r][c] === board[r - 1][c - 1]
+          && board[r][c] === board[r - 2][c - 2]
+          && board[r][c] === board[r - 3][c - 3]
+        ) {
+          return board[r][c];
+        }
+      }
+    }
+  }
+};
+
+const checkDraw = (board) => {
+  for (let r = 0; r < 6; r++) {
+    for (let c = 0; c < 7; c++) {
+      if (board[r][c] === null) {
+        return null;
+      }
+    }
+  }
+  return 'draw';
+};
+
+const checkWinner = (board) => (
+  checkVertical(board)
+    || checkDiagonalRight(board)
+    || checkDiagonalLeft(board)
+    || checkHorizontal(board)
+    || checkDraw(board)
+);
+
+export default checkWinner;
