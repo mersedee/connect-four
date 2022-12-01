ca371b81218294b84a77c569c3a28511901c37e8
mersedee <mersede.rahbar@gmail.com>
2022-12-01 16:33:44 +0330

    add winner status

--- src/App.js
+++ src/App.js
@@ -14,15 +14,22 @@ const createBoard = () => new Array(boardInfo.rows).fill(null).map(
 );
 
 const getColor = (value) => {
-  if (value === boardInfo.player1) return 'bg-red-300';
-  if (value === boardInfo.player2) return 'bg-blue-300';
-  return 'bg-white';
+  if (value === boardInfo.player1) return '!bg-red-300';
+  if (value === boardInfo.player2) return '!bg-blue-300';
+  return '!bg-white';
+};
+
+const generateTitle = (winner, currentPlayer) => {
+  if (winner === boardInfo.player1 || winner === boardInfo.player2) return `${winner} is the winner!`;
+  if (winner === 'draw') return 'Equal!';
+  return `${currentPlayer}'s turn`;
 };
 
 function App() {
   const [board, setBoard] = useState(createBoard());
   const [currentPlayer, setCurrentPlayer] = useState(boardInfo.player1);
   const [winner, setWinner] = useState(null);
+  const [winnerCells, setWinnerCells] = useState([]);
 
   const play = (rowIndex, colIndex) => {
     const current = (currentPlayer === boardInfo.player1) ? boardInfo.player2 : boardInfo.player1;
@@ -34,28 +41,38 @@ function App() {
     setBoard(createBoard());
     setCurrentPlayer(boardInfo.player1);
     setWinner(null);
+    setWinnerCells([]);
+  };
+
+  const getWinnerStyle = (rowIndex, colIndex) => {
+    const item = [rowIndex, colIndex];
+    const itemString = JSON.stringify(item);
+    const isCellInBoard = winnerCells.some((cell) => JSON.stringify(cell) === itemString);
+    const newColor = winner === boardInfo.player1 ? '!bg-red-500' : '!bg-blue-500';
+    return isCellInBoard && `blink ${newColor}`;
   };
 
   useEffect(() => {
-    const win = checkWinner(board);
+    const win = checkWinner(board, setWinnerCells);
     if (win) setWinner(win);
   }, [currentPlayer]);
 
   return (
-    <div className="App p-20">
+    <div className="App px-4 py-10 md:p-20">
       <h1 className="text-4xl text-center font-medium first-letter:uppercase">
-        {currentPlayer}
-        &apos;s turn
+        {generateTitle(winner, currentPlayer)}
       </h1>
 
-      <div className="bg-gray-200 w-fit block mx-auto py-3 px-2 rounded-3xl mt-8">
+      <div className="board bg-gray-200 w-fit block mx-auto py-3 px-2 rounded-3xl mt-8">
         {board.map((rows, rowIndex) => (
-          <div className="flex">
+          <div className="flex" key={rowIndex}>
             {rows.map((col, colIndex) => (
-              <div className="h-24 w-24 flex justify-center items-center">
+              <div className="h-12 w-12 md:h-24 md:w-24 flex justify-center items-center" key={colIndex}>
+                {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
                 <button
                   type="button"
-                  className={`h-20 w-20 bg-white rounded-full m-2 flex justify-center items-center ${getColor(col)}`}
+                  disabled={winner}
+                  className={`board-block h-9 w-9 md:h-20 md:w-20 bg-white rounded-full m-1 md:m-2 flex justify-center items-center ${getColor(col)} ${getWinnerStyle(rowIndex, colIndex)}`}
                   onClick={() => play(rowIndex, colIndex)}
                 />
               </div>
@@ -66,11 +83,6 @@ function App() {
 
       {winner && (
         <div className="text-center">
-          <div className="text-2xl first-letter:uppercase mt-8">
-            {winner}
-            {' '}
-            is winner!
-          </div>
           <button
             type="button"
             className="text-xl rounded-lg mt-8 px-4 py-3 bg-blue-500 text-white"
--- src/helper/checkWinner.js
+++ src/helper/checkWinner.js
@@ -1,4 +1,4 @@
-const checkVertical = (board) => {
+const checkVertical = (board, setWinnerCells) => {
   // Check only if row is 3 or greater
   for (let r = 3; r < 6; r++) {
     for (let c = 0; c < 7; c++) {
@@ -6,6 +6,7 @@ const checkVertical = (board) => {
         if (board[r][c] === board[r - 1][c]
             && board[r][c] === board[r - 2][c]
             && board[r][c] === board[r - 3][c]) {
+          setWinnerCells([[r, c], [r - 1, c], [r - 2, c], [r - 3, c]]);
           return board[r][c];
         }
       }
@@ -13,7 +14,7 @@ const checkVertical = (board) => {
   }
 };
 
-const checkHorizontal = (board) => {
+const checkHorizontal = (board, setWinnerCells) => {
   // Check only if column is 3 or less
   for (let r = 0; r < 6; r++) {
     for (let c = 0; c < 4; c++) {
@@ -23,6 +24,7 @@ const checkHorizontal = (board) => {
           && board[r][c] === board[r][c + 2]
           && board[r][c] === board[r][c + 3]
         ) {
+          setWinnerCells([[r, c], [r, c + 1], [r, c + 2], [r, c + 3]]);
           return board[r][c];
         }
       }
@@ -30,7 +32,7 @@ const checkHorizontal = (board) => {
   }
 };
 
-const checkDiagonalRight = (board) => {
+const checkDiagonalRight = (board, setWinnerCells) => {
   // Check only if row is 3 or greater AND column is 3 or less
   for (let r = 3; r < 6; r++) {
     for (let c = 0; c < 4; c++) {
@@ -40,6 +42,7 @@ const checkDiagonalRight = (board) => {
           && board[r][c] === board[r - 2][c + 2]
           && board[r][c] === board[r - 3][c + 3]
         ) {
+          setWinnerCells([[r, c], [r - 1, c + 1], [r - 2, c + 2], [r - 3, c + 3]]);
           return board[r][c];
         }
       }
@@ -47,7 +50,7 @@ const checkDiagonalRight = (board) => {
   }
 };
 
-const checkDiagonalLeft = (board) => {
+const checkDiagonalLeft = (board, setWinnerCells) => {
   // Check only if row is 3 or greater AND column is 3 or greater
   for (let r = 3; r < 6; r++) {
     for (let c = 3; c < 7; c++) {
@@ -57,6 +60,7 @@ const checkDiagonalLeft = (board) => {
           && board[r][c] === board[r - 2][c - 2]
           && board[r][c] === board[r - 3][c - 3]
         ) {
+          setWinnerCells([[r, c], [r - 1, c - 1], [r - 2, c - 2], [r - 3, c - 3]]);
           return board[r][c];
         }
       }
@@ -75,12 +79,12 @@ const checkDraw = (board) => {
   return 'draw';
 };
 
-const checkWinner = (board) => (
-  checkVertical(board)
-    || checkDiagonalRight(board)
-    || checkDiagonalLeft(board)
-    || checkHorizontal(board)
-    || checkDraw(board)
+const checkWinner = (board, setWinnerCells) => (
+  checkVertical(board, setWinnerCells)
+    || checkDiagonalRight(board, setWinnerCells)
+    || checkDiagonalLeft(board, setWinnerCells)
+    || checkHorizontal(board, setWinnerCells)
+    || checkDraw(board, setWinnerCells)
 );
 
 export default checkWinner;
